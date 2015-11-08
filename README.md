9066f7abd0412b379891b0a2c0824401e4db1181
Paul Sastrasinh <psastras@Pauls-MBP.fios-router.home>
2015-11-08 01:54:52 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## brunch-ts
+Adds TypeScript support to [brunch](http://brunch.io).
+
+Based off of [brunch-typescript](https://github.com/baptistedonaux/brunch-typescript). 
---
+++ index.js
@@ -0,0 +1,56 @@
+var TypeScriptCompiler;
+
+module.exports = TypeScriptCompiler = (function () {
+
+    var exec    = require('child_process'),
+        sysPath = require('path'),
+        fs = require('fs');
+
+    TypeScriptCompiler.prototype.brunchPlugin = true;
+    TypeScriptCompiler.prototype.type = 'javascript';
+    TypeScriptCompiler.prototype.extension = 'ts';
+    TypeScriptCompiler.prototype.pattern = /\.ts(x)?$/;
+
+    function TypeScriptCompiler(config) {
+        this.config = config;
+    }
+
+    TypeScriptCompiler.prototype.compile = function (params, callback) {
+        var opt = (
+            typeof this.config.plugins.brunchTypescript === 'undefined' ?
+            {} :
+            this.config.plugins.brunchTypescript
+        );
+        
+        var cmd = [
+            sysPath.join(__dirname) + '/../.bin/tsc ' + params.path
+        ];
+        
+        // Initialize options as a string
+        if (typeof opt.tscOption === 'undefined') {
+            opt.tscOption = '';
+        }
+
+        // Add support for React JSX files by default
+        if (opt.tscOption.indexOf('--jsx') < 0) {
+            cmd.push('--jsx react');
+        }
+
+        cmd.push(opt.tscOption);
+
+        var child = exec.exec(cmd.join(' '), function (error, stdout, stderr) {
+            outputFile = params.path.replace(/\.[^/.]+$/, ".js");
+            fs.readFile(outputFile, 'utf8', function (err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                params.data = data;
+                return callback(err, params);
+            });
+        });
+        
+    }
+
+    return TypeScriptCompiler;
+
+})();
